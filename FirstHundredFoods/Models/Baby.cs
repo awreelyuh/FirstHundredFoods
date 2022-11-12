4169c014cdd34ed758ca853f8827af442cb569ab
Aurelia Ang <findaurelia@live.com>
2022-11-11 23:50:31 -0500

    Deleted User entity

--- FirstHundredFoods/Models/Baby.cs
+++ FirstHundredFoods/Models/Baby.cs
@@ -5,10 +5,13 @@ namespace FirstHundredFoods.Models
     public class Baby
     {
         [Required]
-        public int Id { get; set; }
+        public int ID { get; set; }
 
         [Required, StringLength(50, MinimumLength = 2)]
-        public string Name { get; set; } = string.Empty;
+        public string FirstName { get; set; } = string.Empty;
+
+        [Required, StringLength(50, MinimumLength = 2)]
+        public string LastName { get; set; } = string.Empty;
 
         [Required, DataType(DataType.Date)]
         public DateTime DateOfBirth { get; set; }
--- FirstHundredFoods/Models/Food.cs
+++ FirstHundredFoods/Models/Food.cs
@@ -5,12 +5,12 @@ namespace FirstHundredFoods.Models
     public class Food
     {
         [Required]
-        public int Id { get; set; }
+        public int ID { get; set; }
 
         [Required, StringLength(50, MinimumLength = 2), RegularExpression(@"^[A-Z]+[a-zA-Z\s]+$")]
         public string Name { get; set; } = string.Empty;
         public bool CommonAllergen { get; set; }
-        public string FoodType { get; set; } = string.Empty;
+        public string Type { get; set; } = string.Empty;
         public bool RequiresCooking { get; set; }
 
     }
