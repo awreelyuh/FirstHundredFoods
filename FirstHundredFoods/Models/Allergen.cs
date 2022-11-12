fca47a74d91b8c2951cfdd6bbdb2b732183446a6
Aurelia Ang <findaurelia@live.com>
2022-11-12 00:50:43 -0500

    Added properties to Allergen.cs

--- FirstHundredFoods/Models/Allergen.cs
+++ FirstHundredFoods/Models/Allergen.cs
@@ -1,6 +1,14 @@
 ﻿namespace FirstHundredFoods.Models
 {
+    public enum Severity
+    {
+        mild, moderate, severe
+    }
+
     public class Allergen
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Severity Severity { get; set; }
     }
 }
