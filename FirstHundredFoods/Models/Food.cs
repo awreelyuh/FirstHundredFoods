076a92ebe882ff866a9144df7cf143010eb31a8b
Aurelia Ang <findaurelia@live.com>
2022-11-18 16:20:35 -0500

    Edited properties of Food entity

--- FirstHundredFoods/Models/Food.cs
+++ FirstHundredFoods/Models/Food.cs
@@ -8,14 +8,12 @@ namespace FirstHundredFoods.Models
     }
     public class Food
     {
-        [Required]
         public int ID { get; set; }
 
         [Required, StringLength(50, MinimumLength = 2), RegularExpression(@"^[A-Z]+[a-zA-Z\s]+$")]
         public string Name { get; set; }
         public bool ContainsAllergen { get; set; }
         public FoodType FoodType { get; set; }
-        public bool RequiresCooking { get; set; }
 
     }
 }
